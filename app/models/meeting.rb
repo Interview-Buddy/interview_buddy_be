5e375f067e19e0d7421edc9bda145c03d42de545
Joseph Downs <jos.w.downs@gmail.com>
2023-06-29 11:18:45 -0600

    Model syntax fix

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,3 @@
 class Meeting < ApplicationRecord
-  has_many :users through :user_meetings
+  has_many :users, through: :user_meetings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  has_many :meetings through :user_meetings
+  has_many :meetings, through: :user_meetings
 end
