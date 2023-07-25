451acfd97959cb93e1104ad05724b0662f2dd133
Colin Reinhart <colinbreinhart@gmail.com>
2023-07-25 15:52:36 -0600

    Delete User

---
+++ app/graphql/mutations/user_delete.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class UserDelete < BaseMutation
+    description "Deletes a user by ID"
+
+    field :user, Types::UserType, null: false
+    # field :errors, [String], null: false
+
+    argument :id, ID, required: true
+
+    def resolve(id:)
+      user = ::User.find(id)
+      raise GraphQL::ExecutionError.new "Error deleting user", extensions: user.errors.to_hash unless user.destroy
+
+      { user: user }
+    end
+  end
+end
