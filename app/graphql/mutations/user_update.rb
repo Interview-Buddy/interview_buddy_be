2ee2e3ffbd4e3c8a07ffa0bcebc0f71ca05e6c65
Colin Reinhart <colinbreinhart@gmail.com>
2023-07-25 14:22:02 -0600

    Update User Mutation

---
+++ app/graphql/mutations/user_update.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Mutations
+  class UserUpdate < BaseMutation
+    description "Updates a user by id"
+
+    field :user, Types::UserType, null: false
+
+    argument :id, ID, required: true
+    argument :first_name, String, required: false
+    argument :last_name, String, required: false
+    argument :email, String, required: false
+    argument :password, String, required: false
+    argument :user_type, String, required: false
+
+    def resolve(id:, **args)
+      user = ::User.find(id)
+      raise GraphQL::ExecutionError.new "Error updating user", extensions: user.errors.to_hash unless user.update(**args)
+
+      { user: user }
+    end
+  end
+end
