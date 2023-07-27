cc8b1e44098f70e10e7ab5c96d535a2c1d6a2289
Joseph Downs <jos.w.downs@gmail.com>
2023-07-27 16:38:54 -0600

    Update models to reflect data type change

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,4 @@
 class Meeting < ApplicationRecord
+  has_many :users, through: :user_meetings
+  enum interview_type: {"technical" => 0, "behavioral" => 1, "both" => 2}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :meetings, through: :user_meetings
+  enum user_type: {"student" => 0, "alum" => 1, "admin" => 2}
 end
