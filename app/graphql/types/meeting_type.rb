51d8ed3b80535cb358d688f32ce8a779805a8661
Joseph Downs <jos.w.downs@gmail.com>
2023-07-27 23:40:54 -0600

    Fuss with data types

--- app/graphql/types/meeting_type.rb
+++ app/graphql/types/meeting_type.rb
@@ -5,10 +5,10 @@ module Types
     field :id, ID, null: false
     field :start_time, String
     field :end_time, String
-    field :interview_type, Int
+    field :interview_type, String
     field :student_id, Int
     field :alum_id, Int
-    field :date, GraphQL::Types::ISO8601Date
+    field :date, GraphQL::Types::ISO8601DateTime
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
