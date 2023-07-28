910a1842df84476f3bbdfa983f02694241239542
Joseph Downs <jos.w.downs@gmail.com>
2023-07-27 22:20:27 -0600

    Update Get meetings

--- app/graphql/types/meeting_type.rb
+++ app/graphql/types/meeting_type.rb
@@ -5,9 +5,9 @@ module Types
     field :id, ID, null: false
     field :start_time, String
     field :end_time, String
-    field :interview_type, Integer
-    field :student_id, Integer
-    field :alum_id, Integer
+    field :interview_type, Int
+    field :student_id, Int
+    field :alum_id, Int
     field :date, GraphQL::Types::ISO8601Date
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
