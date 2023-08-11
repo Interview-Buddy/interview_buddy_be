d1dc8eed61aef2e84134c35b9ba69a686e3f8cb1
Colin Reinhart <colinbreinhart@gmail.com>
2023-08-11 11:51:45 -0600

    Create user mutation tests passing

--- spec/graphql/users/user_graphql_spec.rb
+++ spec/graphql/users/user_graphql_spec.rb
@@ -35,7 +35,17 @@ RSpec.describe Mutations::CreateUser, type: :request do
         }
 
         expect(response).to have_http_status(:success)
+        json_response = JSON.parse(response.body)
+        data = json_response['data']['createUser']['user']
 
+        expect(data['firstName']).to eq("Lloyd")
+        expect(data['lastName']).to eq("Christmas")
+        expect(data['email']).to eq("dumb@aol.com")
+        expect(data['password']).to eq("password")
+        expect(data['userType']).to eq("student")
+        expect(data['pronouns']).to eq("he/him")
+        expect(data['displayName']).to eq("Lloyd Christmas")
+        expect(data['company']).to eq("I Got Worms")        
     end
 
 end
