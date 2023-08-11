1f4177863ef1978427836a8310f0e69f825632c2
Colin Reinhart <colinbreinhart@gmail.com>
2023-08-11 14:17:17 -0600

    Update User Test Passing

--- spec/graphql/users/user_graphql_spec.rb
+++ spec/graphql/users/user_graphql_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Mutations::CreateUser, type: :request do
-    
+
     it 'can use graphql to create a user' do
         post '/graphql', params: {
             query: <<-GRAPHQL
@@ -45,7 +45,34 @@ RSpec.describe Mutations::CreateUser, type: :request do
         expect(data['userType']).to eq("student")
         expect(data['pronouns']).to eq("he/him")
         expect(data['displayName']).to eq("Lloyd Christmas")
-        expect(data['company']).to eq("I Got Worms")        
+        expect(data['company']).to eq("I Got Worms")
     end
 
+    it 'can update user' do
+        expect(User.count).to eq (0)
+        user = User.create!(first_name: "Lloyd", last_name: "Christmas", email: "dumb@aol.com", password: "password", user_type: 0, pronouns: "he/him", display_name: "Lloyd Christmas", company: "I Got Worms")
+        expect(User.count).to eq(1)
+
+        post '/graphql', params: {
+            query: <<-GRAPHQL
+            mutation {
+                updateUser(input:{
+                    id: #{user.id},
+                    firstName: "Floyd"
+                 }) {
+                    user{
+                        firstName
+                        email
+                    }
+                }
+            }
+            GRAPHQL
+        }
+
+        expect(response).to have_http_status(:success)
+        json_response = JSON.parse(response.body)
+        data = json_response['data']['updateUser']['user']
+
+        expect(data['firstName']).to eq("Floyd")
+    end
 end
