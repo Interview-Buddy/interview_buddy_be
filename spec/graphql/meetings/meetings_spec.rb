55e4383f9f372b311dca111698b6e9f5bdf7d312
Joseph Downs <jos.w.downs@gmail.com>
2023-08-21 14:21:30 -0600

    Cleanup

--- spec/graphql/meetings/meetings_spec.rb
+++ spec/graphql/meetings/meetings_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Mutations::CreateMeeting, type: :request do
   it 'can create a meeting' do
-    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com")
+    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", uuid: "ytehr", user_type: "alum", company: "NASA", email: "test@test.com")
     post "/graphql", params: {
       query: <<-GRAPHQL
 
@@ -33,8 +33,8 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
   end
 
   it 'can find all meetings by user id' do
-    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com")
-    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com")
+    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com", uuid: "pwwes")
+    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com", uuid: "uulrnh")
     meeting1 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
     meeting2 = Meeting.create!(start_time: "9", end_time: "6", interview_type: "both", student_id: "#{josh.id}", user_id: "#{carl.id}", date: "11/12/22")
     meeting3 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
@@ -45,8 +45,8 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
     post "/graphql", params: {
       query: <<-GRAPHQL
         {
-	        meetingByUser(userId: #{josh.id}) {
-	        	id
+          meetingByUser(userId: #{josh.id}) {
+            id
             startTime
             endTime
             interviewType
@@ -54,7 +54,7 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
             userId
             date
             title
-        	}
+          }
         }
       GRAPHQL
     }
@@ -65,8 +65,8 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
   end
 
   it 'can find meeting by type' do
-    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com")
-    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com")
+    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com", uuid: "uuid")
+    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com", uuid: "uuod")
     meeting1 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
     meeting2 = Meeting.create!(start_time: "9", end_time: "6", interview_type: "both", student_id: "#{josh.id}", user_id: "#{carl.id}", date: "11/12/22")
     meeting3 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
@@ -77,8 +77,8 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
     post "/graphql", params: {
       query: <<-GRAPHQL
         {
-	        meetingByInterviewType(interviewType: "technical") {
-	        	id
+          meetingByInterviewType(interviewType: "technical") {
+            id
             startTime
             endTime
             interviewType
@@ -86,7 +86,7 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
             userId
             date
             title
-        	}
+          }
         }
       GRAPHQL
     }
@@ -97,8 +97,8 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
   end
 
   it 'can find a meeting by date' do
-    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com")
-    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com")
+    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com", uuid: "uuuid")
+    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com", uuid: "uudlajg")
     meeting1 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
     meeting2 = Meeting.create!(start_time: "9", end_time: "6", interview_type: "both", student_id: "#{josh.id}", user_id: "#{carl.id}", date: "07/19/1988")
     meeting3 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
@@ -109,8 +109,8 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
     post "/graphql", params: {
       query: <<-GRAPHQL
         {
-	        meetingsByDate(date: "11/12/22") {
-	        	id
+          meetingsByDate(date: "11/12/22") {
+            id
             startTime
             endTime
             interviewType
@@ -118,7 +118,7 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
             userId
             date
             title
-        	}
+          }
         }
       GRAPHQL
     }
