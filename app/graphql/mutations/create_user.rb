9c7b20d78f06cd5475417149de07d39c9de7266a
Joseph Downs <jos.w.downs@gmail.com>
2023-08-21 13:30:44 -0600

    Edit createUsermutation to include uuid and remove password

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -3,7 +3,7 @@ module Mutations
     argument :first_name, String, required: true
     argument :last_name, String, required: true
     argument :email, String, required: true
-    argument :password, String, required: true
+    argument :uuid, String, required: true
     argument :user_type, Integer, required: true
     argument :display_name, String, required: false
     argument :pronouns, String, required: false
@@ -12,8 +12,8 @@ module Mutations
     field :user, Types::UserType, null: false
     field :errors, [String], null: false
 
-    def resolve(first_name:, last_name:, password:, user_type:, email:, display_name:, pronouns:, company:)
-      user = User.new(first_name: first_name, last_name: last_name, password: password, user_type: user_type, 
+    def resolve(first_name:, last_name:, uuid:, user_type:, email:, display_name:, pronouns:, company:)
+      user = User.new(first_name: first_name, last_name: last_name, uuid: uuid, user_type: user_type, 
         email: email, pronouns: pronouns, display_name: display_name, company: company)
       if user.save
         {
