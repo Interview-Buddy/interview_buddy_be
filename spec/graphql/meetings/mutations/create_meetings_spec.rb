1b9e0ba1813f05ce09cb91e79755585bffb9f30f
Joseph Downs <jos.w.downs@gmail.com>
2023-08-22 17:03:02 -0600

    Space concerns

--- spec/graphql/meetings/mutations/create_meetings_spec.rb
+++ spec/graphql/meetings/mutations/create_meetings_spec.rb
@@ -31,4 +31,35 @@ RSpec.describe Mutations::CreateMeeting, type: :request do
     expect(data['title']).to eq("Interview with Colin4Prez")
 
   end
+
+  it 'can create a meeting using first and last name' do
+    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", user_type: "alum", company: "Tesla", email: "test@aol.com", uuid: "uulrnh")
+
+    post "/graphql", params: {
+      query: <<-GRAPHQL
+
+        mutation {
+          createMeeting(
+              input : {
+                  startTime: "11"
+                  endTime: "12"
+                  interviewType: 1
+                  userId: #{josh.id}
+                  date: "08/20/2023"
+                  studentId: null
+                  title: null
+              }
+          ) {
+              meeting{
+                  id
+                  title
+              }
+          }
+      }
+      GRAPHQL
+    }
+    expect(response).to have_http_status(:success)
+    data = JSON.parse(response.body)['data']['createMeeting']['meeting']
+    expect(data['title']).to eq("Interview with Joseph Downs")
+  end
 end
