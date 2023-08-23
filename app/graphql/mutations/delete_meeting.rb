42fed655d3570af32d97d1f7cc70572ace294cd0
Joseph Downs <jos.w.downs@gmail.com>
2023-08-23 13:48:32 -0600

    Create deleteMeeting mutation

---
+++ app/graphql/mutations/delete_meeting.rb
@@ -0,0 +1,16 @@
+module Mutations
+  class DeleteMeeting < BaseMutation
+    argument :id, ID, required: true
+    field :success, Boolean
+
+    def resolve(id:)
+      meeting = Meeting.find(id)
+      if meeting.destroy
+        { success: true }
+      else
+        { success: false }
+      end
+    end
+  end
+  
+end
