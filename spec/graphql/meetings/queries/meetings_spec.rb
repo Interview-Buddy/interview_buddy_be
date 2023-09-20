b76b987536658fc862d2993654cecc8040aa2da9
Joseph Downs <jos.w.downs@gmail.com>
2023-09-20 10:17:35 -0600

    Add test for user and month

--- spec/graphql/meetings/queries/meetings_spec.rb
+++ spec/graphql/meetings/queries/meetings_spec.rb
@@ -126,4 +126,38 @@ RSpec.describe 'query meetings', type: :request do
 
     expect(data.count).to eq(5)
   end
+
+  it 'can find a meeting by user and month' do
+    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com", uuid: "uuuid")
+    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com", uuid: "uudlajg")
+    meeting1 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
+    meeting2 = Meeting.create!(start_time: "9", end_time: "6", interview_type: "both", student_id: "#{josh.id}", user_id: "#{carl.id}", date: "11/11/22")
+    meeting3 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "12/12/22")
+    meeting4 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "12/11/22")
+    meeting5 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "01/12/23")
+    meeting6 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "01/12/23")
+
+    post "/graphql", params: {
+      query: <<-GRAPHQL
+        {
+          meetingsByUserAndMonth(uuid: "uuuid", month: "11") {
+            id
+            startTime
+            endTime
+            interviewType
+            studentId
+            userId
+            date
+            title
+          }
+        }
+      GRAPHQL
+    }
+
+    expect(response).to have_http_status(:success)
+    data = JSON.parse(response.body)['data']['meetingsByUserAndMonth']
+
+    expect(data.count).to be(1)
+
+  end
 end
