40c65139ea904032f50a2aa755a648afc2f24c62
Joseph Downs <jos.w.downs@gmail.com>
2023-09-20 12:06:16 -0600

    Write tests for student query

--- spec/graphql/meetings/queries/meetings_spec.rb
+++ spec/graphql/meetings/queries/meetings_spec.rb
@@ -159,4 +159,38 @@ RSpec.describe 'query meetings', type: :request do
     expect(data.count).to be(1)
     expect(data[0]["id"]).to eq("#{meeting2.id}")
   end
+
+  it 'can find a meeting by student' do
+    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com", uuid: "uuuid")
+    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com", uuid: "uudlajg")
+    meeting1 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
+    meeting2 = Meeting.create!(start_time: "9", end_time: "6", interview_type: "both", student_id: "#{josh.id}", user_id: "#{carl.id}", date: "11/11/22")
+    meeting3 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "12/12/22")
+    meeting4 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "12/11/22")
+    meeting5 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "01/12/23")
+    meeting6 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "01/12/23")
+
+    post "/graphql", params: {
+      query: <<-GRAPHQL
+        {
+          meetingsByStudent(studentId: #{josh.id}) {
+            id
+            startTime
+            endTime
+            interviewType
+            studentId
+            userId
+            date
+            title
+          }
+        }
+      GRAPHQL
+    }
+
+    expect(response).to have_http_status(:success)
+    data = JSON.parse(response.body)['data']['meetingsByStudent']
+    expect(data.count).to be(1)
+    expect(data[0]["student_id"]).to eq("#{josh.id}")
+  end
+
 end
