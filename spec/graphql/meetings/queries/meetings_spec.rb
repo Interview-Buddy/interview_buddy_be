6aff9b9e9ce11f68a15206c7fcc524fe15163030
Colin Reinhart <colinbreinhart@gmail.com>
2024-01-18 10:35:07 -0700

    Add Available Meetings Query

--- spec/graphql/meetings/queries/meetings_spec.rb
+++ spec/graphql/meetings/queries/meetings_spec.rb
@@ -227,4 +227,35 @@ RSpec.describe 'query meetings', type: :request do
     expect(data[1]["interviewType"]).to eq("behavioral")
   end
 
+    it 'can get all available meetings' do
+    carl = User.create!(first_name: "Colin", last_name: "R", pronouns: "he/him", display_name: "Colin4Prez", user_type: "alum", company: "NASA", email: "test@test.com", uuid: "pwwes")
+    josh = User.create!(first_name: "Joseph", last_name: "Downs", pronouns: "he/him", display_name: "joshy", user_type: "alum", company: "Tesla", email: "test@aol.com", uuid: "uulrnh")
+    meeting1 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: nil, user_id: "#{josh.id}", date: "11/12/22")
+    meeting2 = Meeting.create!(start_time: "9", end_time: "6", interview_type: "both", student_id: "#{josh.id}", user_id: "#{carl.id}", date: "11/12/22")
+    meeting3 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
+    meeting4 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
+    meeting5 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: nil, user_id: "#{josh.id}", date: "11/12/22")
+    meeting6 = Meeting.create!(start_time: "4", end_time: "5", interview_type: "technical", student_id: "#{carl.id}", user_id: "#{josh.id}", date: "11/12/22")
+
+    post "/graphql", params: {
+      query: <<-GRAPHQL
+        {
+          availableMeetings {
+            id
+            startTime
+            endTime
+            interviewType
+            studentId
+            userId
+            date
+            title
+          }
+        }
+      GRAPHQL
+    }
+
+    expect(response).to have_http_status(:success)
+    data = JSON.parse(response.body)['data']['availableMeetings']
+    expect(data.count).to eq(2)
+  end
 end
